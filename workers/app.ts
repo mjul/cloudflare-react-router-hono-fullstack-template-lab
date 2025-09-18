ab6096e5cb6867f0b7936a351ecc31cacdc19363
Martin Jul <martin@mjul.com>
2025-09-18 21:34:02 +0200

    Add a skeleton /api/documents endpoint

--- workers/app.ts
+++ workers/app.ts
@@ -1,20 +1,24 @@
-import { Hono } from "hono";
-import { createRequestHandler } from "react-router";
+import {Hono} from "hono";
+import {createRequestHandler} from "react-router";
 
 const app = new Hono();
 
 // This is the main entry point for the Cloudflare Worker, the back-end app.
 // Add more routes here
 
-app.get("*", (c) => {
-  const requestHandler = createRequestHandler(
-    () => import("virtual:react-router/server-build"),
-    import.meta.env.MODE,
-  );
+app.get("/api/documents", (c) => {
+    return c.json({message: "This is the documents endpoint"});
+});
 
-  return requestHandler(c.req.raw, {
-    cloudflare: { env: c.env, ctx: c.executionCtx },
-  });
+app.get("*", (c) => {
+    const requestHandler = createRequestHandler(
+        () => import("virtual:react-router/server-build"),
+        import.meta.env.MODE,
+    );
+
+    return requestHandler(c.req.raw, {
+        cloudflare: {env: c.env, ctx: c.executionCtx},
+    });
 });
 
 export default app;
